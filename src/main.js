3f4b7466c136eb012aab2c13ffd2728356dfeb6d
wen.you <wen.you@nextfortune.tw>
2021-11-22 17:15:43 +0800

    add style setting

--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-createApp(App).use(store).use(router).mount('#app')
+import VueGoogleMaps from "@fawmi/vue-google-maps";
+
+import "./assets/scss/reset.css";
+import "./assets/scss/basic.scss";
+import "./assets/fonts/font.css";
+import "./assets/icon/style.css";
+
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(VueGoogleMaps, {
+    load: {
+      key: "AIzaSyBD5zZfLsBKn7bZThJUoP8wkJn6shPhXkM",
+    },
+  })
+  .mount("#app");
