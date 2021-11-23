3a1da735d43f775640b51d2ac4d7300cd59a1d01
wen.you <wen.you@nextfortune.tw>
2021-11-23 17:01:31 +0800

    add npm run deploy & axios & jssha

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:wenyo/da_bus.git master:gh-pages
+git push -f https://github.com/wenyo/da_bus.git master:gh-pages
 # 將dist資料夾中的內容推送至遠端taiwan_travel的gh-pages分支中，並強制無條件將舊有的內容取代成目前的內容（指令 git push -f)
 cd -
